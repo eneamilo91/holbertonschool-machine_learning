1856d75a7f107c055ba9e6363818d3ec8497acf5
eneamilo91 <eneamilo514@gmail.com>
2024-12-20 00:45:56 +0000

    added new files

--- pipeline/pandas/9-fill.py
+++ pipeline/pandas/9-fill.py
@@ -3,22 +3,18 @@
 New code fills in the missing data points in the DataFrame
 """
 
-import pandas as pd
-from_file = __import__('2-from_file').from_file
 
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+def fill(df):
+    """ performs actions """
+    df = df.drop(columns=['Weighted_Price'])
 
+    df['Close'].fillna(method='pad', inplace=True)
 
-df = df.drop(columns=['Weighted_Price'])
+    df['High'].fillna(df.Close, inplace=True)
+    df['Low'].fillna(df.Close, inplace=True)
+    df['Open'].fillna(df.Close, inplace=True)
 
-df['Close'].fillna(method='pad', inplace=True)
+    df['Volume_(BTC)'].fillna(value=0, inplace=True)
+    df['Volume_(Currency)'].fillna(value=0, inplace=True)
 
-df['High'].fillna(df.Close, inplace=True)
-df['Low'].fillna(df.Close, inplace=True)
-df['Open'].fillna(df.Close, inplace=True)
-
-df['Volume_(BTC)'].fillna(value=0, inplace=True)
-df['Volume_(Currency)'].fillna(value=0, inplace=True)
-
-print(df.head())
-print(df.tail())
+    return (df)
