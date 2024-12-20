6bc4fcfb12f3de62c513604be0dc8c62f6d2b9e9
eneamilo91 <eneamilo514@gmail.com>
2024-12-20 00:51:35 +0000

    added files

--- pipeline/pandas/10-index.py
+++ pipeline/pandas/10-index.py
@@ -3,11 +3,9 @@
 Index the DataFrame on the Timestamp column
 """
 
-import pandas as pd
-from_file = __import__('2-from_file').from_file
 
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+def index(df):
+    """ sets index """
+    df = df.set_index('Timestamp')
 
-df = df.set_index('Timestamp')
-
-print(df.tail())
+    return (df)
