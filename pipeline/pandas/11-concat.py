af6bcb5f52aced557fda19d8da9e0d688909cf8d
eneamilo91 <eneamilo514@gmail.com>
2024-12-20 00:54:52 +0000

    added files

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -3,13 +3,16 @@
 Index the DataFrame on the Timestamp columns and concatenates them
 """
 
+import pandas as pd
+index = __import__('10-index').index
+
 
 def concat(df1, df2):
     """ concatenates dataframes """
     df2 = df2.loc[df2['Timestamp'] <= 1417411920]
 
-    df1 = df1.set_index('Timestamp')
-    df2 = df2.set_index('Timestamp')
+    df1 = df1.index('Timestamp')
+    df2 = df2.index('Timestamp')
 
     df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
 
