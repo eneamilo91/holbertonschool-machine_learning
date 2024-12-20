e9bd5835b9e939ea3db52b41e90d384ccbbdf351
eneamilo91 <eneamilo514@gmail.com>
2024-12-20 00:37:47 +0000

    added new files

--- pipeline/pandas/6-flip_switch.py
+++ pipeline/pandas/6-flip_switch.py
@@ -4,12 +4,9 @@ Transpose the rows and columns and then sorts the data
     in reverse chronological order
 """
 
-from_file = __import__('2-from_file').from_file
-
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-
 
 def flip_switch(df):
+    """ sorts the data and transposes """
     df = df.sort_values(by='Timestamp', ascending=False).T
 
     return (df)
