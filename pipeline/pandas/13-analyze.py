15c1386bd4f7132923d6de493454386a1caf0743
eneamilo91 <eneamilo514@gmail.com>
2024-12-20 01:36:30 +0000

    added files

--- pipeline/pandas/13-analyze.py
+++ pipeline/pandas/13-analyze.py
@@ -4,10 +4,10 @@ Calculate descriptive statistics for all columns except Timestamp
 """
 
 import pandas as pd
-from_file = __import__('2-from_file').from_file
 
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
-stats = df.drop(columns=['Timestamp']).describe()
+def analyze(df):
+    """ computes descriptive stats """
+    stats = df.drop(columns=['Timestamp']).describe()
 
-print(stats)
+    return (stats)
