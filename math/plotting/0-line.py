df3e1a4b09e962a76073bae2bf987aae1bc295f2
eneamilo91 <eneamilo514@gmail.com>
2024-12-21 17:11:34 +0000

    added new file

---
+++ math/plotting/0-line.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" module of a function to plot a line graph"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def line():
+    """ function that plots a line graph """
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.plot(y, 'r-')
+    plt.xlim((0, 10))
+    plt.show()
---
+++ math/plotting/0-main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+line = __import__('0-line').line
+
+line()
+hbt-ml@Holberton-ML:~$ ./0-main.py
