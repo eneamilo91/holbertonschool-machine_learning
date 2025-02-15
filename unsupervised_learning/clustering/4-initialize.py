8f87a431bef4485398ecd3395f64a10e0b42ee1d
eneamilo91 <eneamilo514@gmail.com>
2025-02-15 22:54:50 +0000

    added new file

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -1,39 +1,32 @@
 #!/usr/bin/env python3
-"""GMM function """
-
+"""Initialize Gaussian Mixture Model Module"""
 import numpy as np
 kmeans = __import__('1-kmeans').kmeans
 
 
 def initialize(X, k):
-    """
-    initializes variables for a Gaussian Mixture Model
-    Args:
-        X: numpy.ndarray of shape (n, d) containing the data set
-        k: positive integer containing the number of clusters
+    """Initializes variables for a Gaussian Mixture Model:
+
+    X is a numpy.ndarray of shape (n, d) containing the data set
+    k is a positive integer containing the number of clusters
+    You are not allowed to use any loops
     Returns: pi, m, S, or None, None, None on failure
-            pi: numpy.ndarray of shape (k,) containing the priors
-                for each cluster initialized evenly
-            m: numpy.ndarray of shape (k, d) containing the centroid
-                means for each cluster, initialized with K-means
-            s: numpy.ndarray of shape (k, d, d) containing the covariance
-               matrices for each cluster, initialized as identity matrices
+    pi is a numpy.ndarray of shape (k,) containing the priors for each
+    cluster, initialized evenly
+    m is a numpy.ndarray of shape (k, d) containing the centroid means for
+    each cluster, initialized with K-means
+    S is a numpy.ndarray of shape (k, d, d) containing the covariance matrices
+    for each cluster, initialized as identity matrices
     """
 
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None
-    if not isinstance(k, int) or k < 1:
+
+    if not isinstance(k, int) or k <= 0:
         return None, None, None
 
-    n, d = X.shape
-
-    # priors for each cluster, initialized evenly
-    phi = np.ones(k)/k
-
-    # centroid means for each cluster, initialized with K-means
+    pi = np.repeat(1.0 / k, k)
     m, _ = kmeans(X, k)
+    S = np.tile(np.identity(X.shape[1]), (k, 1, 1))
 
-    # covariance matrices for each cluster, initialized as identity matrices
-    S = np.tile(np.identity(d), (k, 1)).reshape(k, d, d)
-
-    return phi, m, S
+    return pi, m, S
--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -1,81 +1,77 @@
 #!/usr/bin/env python3
-""" Bayesian Information Criterion """
-
+"""Bayesian Information Criterion Module"""
 import numpy as np
 expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
-    """
-    BIC function
-    Args:
-        X: numpy.ndarray of shape (n, d) containing the data set
-        kmin: positive integer containing the minimum number of clusters
-              to check for (inclusive)
-        kmax: positive integer containing the maximum number of clusters
-              to check for (inclusive)
-        iterations: Positive integer containing the maximum number of
-                    iterations for the EM algorithm
-        tol: non-negative float containing the tolerance for the EM algorithm
-        verbose: boolean that determines if the EM algorithm should print
-                 information to the standard output
+    """Finds the best number of clusters for a GMM using the Bayesian
+    Information Criterion:
+
+    X is a numpy.ndarray of shape (n, d) containing the data set
+    kmin is a positive integer containing the minimum number of clusters to
+    check for (inclusive)
+    kmax is a positive integer containing the maximum number of clusters to
+    check for (inclusive)
+    If kmax is None, kmax should be set to the maximum number of clusters
+    possible
+    iterations is a positive integer containing the maximum number of
+    iterations for the EM algorithm
+    tol is a non-negative float containing the tolerance for the EM algorithm
+    verbose is a boolean that determines if the EM algorithm should print
+    information to the standard output
+
     Returns: best_k, best_result, l, b, or None, None, None, None on failure
-             best_k: best value for k based on its BIC
-             best_result : tuple containing pi, m, S
-                           pi: numpy.ndarray of shape (k,) containing the
-                               luster priors for the best number of clusters
-                           m: numpy.ndarray of shape (k, d) containing the
-                              centroid means for the best number of clusters
-                           S: numpy.ndarray of shape (k, d, d) containing the
-                              covariance matrices for the best number of
-                              clusters
-             l: numpy.ndarray of shape (kmax - kmin + 1) containing the log
-                likelihood for each cluster size tested
-             b: numpy.ndarray of shape (kmax - kmin + 1) containing the BIC
-                value for each cluster size tested
+    best_k is the best value for k based on its BIC
+    best_result is tuple containing pi, m, S
+    pi is a numpy.ndarray of shape (k,) containing the cluster priors for the
+    best number of clusters
+    m is a numpy.ndarray of shape (k, d) containing the centroid means for the
+    best number of clusters
+    S is a numpy.ndarray of shape (k, d, d) containing the covariance matrices
+    for the best number of clusters
+    l is a numpy.ndarray of shape (kmax - kmin + 1) containing the log
+    lh for each cluster size tested
+    b is a numpy.ndarray of shape (kmax - kmin + 1) containing the BIC value
+    for each cluster size tested
+    Use: BIC = p * ln(n) - 2 * l
+    p is the number of parameters required for the model
+    n is the number of data points used to create the model
+    l is the log lh of the model
     """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-        return None, None, None, None
-    if type(kmin) != int or kmin <= 0 or kmin >= X.shape[0]:
-        return None, None, None, None
-    if type(kmax) != int or kmax <= 0 or kmax >= X.shape[0]:
-        return None, None, None, None
-    if kmin >= kmax:
-        return None, None, None, None
-    if type(iterations) != int or iterations <= 0:
-        return None, None, None, None
-    if type(tol) != float or tol <= 0:
-        return None, None, None, None
-    if type(verbose) != bool:
-        return None, None, None, None
 
-    k_best = []
-    best_res = []
-    logl_val = []
-    bic_val = []
-    n, d = X.shape
-    for k in range(kmin, kmax + 1):
-        pi, m, S,  _, log_l = expectation_maximization(X, k, iterations, tol,
+    try:
+        if kmax == 1:
+            return None, None, None, None
+        n, d = X.shape
+        if kmax is None:
+            kmax = n
+            if kmax >= kmin:
+                return None, None, None, None
+
+        k_history = list(range(kmin, kmax+1))
+        results_history = []
+        lh_history = []
+        bic_history = []
+
+        for k in range(kmin, kmax+1):
+            pi, m, S, g, lh = expectation_maximization(X, k, iterations, tol,
                                                        verbose)
-        k_best.append(k)
-        best_res.append((pi, m, S))
-        logl_val.append(log_l)
 
-        # Formula pf paramaters: https://bit.ly/33Cw8lH
-        # code based on gaussian mixture source code n_parameters source code
-        cov_params = k * d * (d + 1) / 2.
-        mean_params = k * d
-        p = int(cov_params + mean_params + k - 1)
+            if pi is None or m is None or S is None or g is None or lh is None:
+                return None, None, None, None
 
-        # Formula for this task BIC = p * ln(n) - 2 * l
-        bic = p * np.log(n) - 2 * log_l
-        bic_val.append(bic)
+            num_parameters = k + k * d + k * d * (d + 1) // 2 - 1
+            bic = num_parameters * np.log(n) - 2 * lh
 
-    bic_val = np.array(bic_val)
-    logl_val = np.array(logl_val)
-    best_val = np.argmin(bic_val)
+            lh_history.append(lh)
+            results_history.append((pi, m, S))
+            bic_history.append(bic)
 
-    k_best = k_best[best_val]
-    best_res = best_res[best_val]
+        min_bic_index = np.argmin(bic_history)
+        best_k = k_history[min_bic_index]
+        best_result = results_history[min_bic_index]
 
-    return k_best, best_res, logl_val, bic_val
+        return best_k, best_result, np.array(lh_history), np.array(bic_history)
+    except Exception:
+        return None, None, None, None
