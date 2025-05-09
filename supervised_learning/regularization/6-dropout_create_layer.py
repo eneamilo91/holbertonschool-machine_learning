7d203f33a38e2c5053b0a4c4ca8d33a05b09a70e
eneamilo91 <eneamilo514@gmail.com>
2025-05-09 11:46:26 +0000

    added files

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -1,33 +1,31 @@
 #!/usr/bin/env python3
-'''Moudulus that creates a layer of NN using droput'''
-import tensorflow.compat.v1 as tf
+'''Module that creates a Dense layer with dropout using TensorFlow 2.x'''
+import tensorflow as tf
 
 
 def dropout_create_layer(prev, n, activation, keep_prob):
     '''
-    Function that creates a layer of a neural network using dropout
+    Creates a layer of a neural network using dropout.
 
     Parameters
     ----------
-    prev : TYPE tensor
-        DESCRIPTION. Output of the previus layer
-    n : TYPE int
-        DESCRIPTION. Number of nodes
-    activation : TYPE str
-        DESCRIPTION. Activation functions that should be used in the layer
-    keep_prob : TYPE float
-        DESCRIPTION. Probability of the node to be kept
+    prev : tf.Tensor
+        Output from the previous layer.
+    n : int
+        Number of neurons in the layer.
+    activation : callable
+        Activation function to apply (e.g., tf.nn.relu).
+    keep_prob : float
+        Probability of keeping a node (i.e., 1 - dropout rate).
 
     Returns
     -------
-    The output of the new layer.
-
+    tf.Tensor
+        Output of the new layer after applying dropout.
     '''
-    init = tf.keras.initializers.VarianceScaling(scale=2.0,
-                                                 mode=("fan_avg"))
-    drop = tf.layers.Dropout(rate=keep_prob)
-    layer = tf.layers.Dense(n,
-                            activation=activation,
-                            kernel_initializer=init,
-                            kernel_regularizer=drop)(prev)
-    return layer
+    init = tf.keras.initializers.VarianceScaling(scale=2.0, mode="fan_avg")
+    dense = tf.keras.layers.Dense(units=n,
+                                  activation=activation,
+                                  kernel_initializer=init)(prev)
+    dropout = tf.keras.layers.Dropout(rate=1 - keep_prob)(dense)
+    return dropout
