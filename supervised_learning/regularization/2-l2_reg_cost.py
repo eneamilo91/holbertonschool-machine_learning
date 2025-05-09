e5f1190c53ea805a8d1e3c7ce05f561b9b042bc4
eneamilo91 <eneamilo514@gmail.com>
2025-05-09 11:57:04 +0000

    added files

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -1,25 +1,13 @@
 #!/usr/bin/env python3
-'''
-Module that calculates the cost of a neural network with L2 regularization
-'''
+"""
+Task 2
+"""
+
 import tensorflow as tf
 
 
 def l2_reg_cost(cost, model):
-    '''
-    Calculates total cost including L2 regularization losses.
-
-    Parameters
-    ----------
-    model : tf.keras.Model
-        The Keras model containing regularized layers.
-    base_cost : tf.Tensor
-        The base loss (e.g., cross-entropy) without L2.
-
-    Returns
-    -------
-    tf.Tensor
-        Total cost including L2 regularization.
-    '''
-    reg_losses = tf.add_n(model.losses)
-    return cost + reg_losses
+    """
+    """
+    regularization_loss = model.losses
+    return cost + regularization_loss
--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -1,31 +1,27 @@
 #!/usr/bin/env python3
-'''Module that creates a Dense layer with dropout using TensorFlow 2.x'''
+"""
+Task 6 : dropout
+"""
 import tensorflow as tf
 
 
-def dropout_create_layer(prev, n, activation, keep_prob,training=True):
-    '''
-    Creates a layer of a neural network using dropout.
+def dropout_create_layer(prev, n, activation, keep_prob, training=True):
+    """
 
-    Parameters
-    ----------
-    prev : tf.Tensor
-        Output from the previous layer.
-    n : int
-        Number of neurons in the layer.
-    activation : callable
-        Activation function to apply (e.g., tf.nn.relu).
-    keep_prob : float
-        Probability of keeping a node (i.e., 1 - dropout rate).
+    """
 
-    Returns
-    -------
-    tf.Tensor
-        Output of the new layer after applying dropout.
-    '''
-    init = tf.keras.initializers.VarianceScaling(scale=2.0, mode="fan_avg")
-    dense = tf.keras.layers.Dense(units=n,
-                                  activation=activation,
-                                  kernel_initializer=init)(prev)
-    dropout = tf.keras.layers.Dropout(rate=1 - keep_prob)(dense)
-    return dropout
+    # Initialize layer weights using He initialization
+    init = tf.keras.initializers.VarianceScaling(scale=2.0, mode='fan_avg')
+
+    # Create dense layer
+    dense_layer = tf.keras.layers.Dense(units=n, activation=activation,
+                                        kernel_initializer=init)
+
+    # Apply dense layer to previous layer
+    output = dense_layer(prev)
+
+    # Apply dropout regularization
+    dropout_layer = tf.keras.layers.Dropout(rate=1-keep_prob)
+    output = dropout_layer(output, training=training)
+
+    return output
